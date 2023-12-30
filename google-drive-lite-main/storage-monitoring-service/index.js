c57621b6b34bfa6c533979085b67bede7ede7453
salehaahmed14 <sahmed.bese21seecs@seecs.edu.pk>
2023-12-30 16:56:53 +0500

    Update index.js

--- google-drive-lite-main/storage-monitoring-service/index.js
+++ google-drive-lite-main/storage-monitoring-service/index.js
@@ -6,7 +6,7 @@ const { Image } = require('./db'); // Import the Image model from the db module
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 
