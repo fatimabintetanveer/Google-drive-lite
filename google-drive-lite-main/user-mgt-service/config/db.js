4abf2d0facc786cbe30affb3c645bc0f558f6029
salehaahmed14 <sahmed.bese21seecs@seecs.edu.pk>
2023-12-30 17:16:57 +0500

    Update db.js

--- google-drive-lite-main/user-mgt-service/config/db.js
+++ google-drive-lite-main/user-mgt-service/config/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://tammy:pass1234@cluster0.cd9uv7i.mongodb.net/gdrive_lite?retryWrites=true&w=majority').catch(error => console.log(error));
+mongoose.connect('mongodb://127.0.0.1:27017/gdrive_lite', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).catch(error => console.log(error));
 
 const User = mongoose.model('users', new mongoose.Schema({username: String, password: { type: String, select: false }, admin: Boolean, consumed: Number}));
 
