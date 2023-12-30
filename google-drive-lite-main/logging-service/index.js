96a664d3ed0db96daf6875f17697529aa9b98274
Fatima Binte Tanveer <fatimabintetanveer@gmail.com>
2023-12-30 16:23:06 +0500

    Modify Logging service

--- google-drive-lite-main/logging-service/index.js
+++ google-drive-lite-main/logging-service/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const logs_router = require('./logs');
 
 const app = express();
-const port = 8080; // Set the desired port number
+const port = process.env.PORT || 8080;
 
 app.use('/logs', logs_router);
 
