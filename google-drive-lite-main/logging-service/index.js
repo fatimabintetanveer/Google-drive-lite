6f6ffee4e60c8de65abbebbc18a5a7209679436e
Fatima Binte Tanveer <fatimabintetanveer@gmail.com>
2023-12-30 15:58:25 +0500

    Modify loggig service

--- google-drive-lite-main/logging-service/index.js
+++ google-drive-lite-main/logging-service/index.js
@@ -4,13 +4,14 @@ const express = require('express');
 const logs_router = require('./logs');
 
 const app = express();
+const port = 8080; // Set the desired port number
 
 app.use('/logs', logs_router);
 
 app.get('/*', (req, res) => {
-    res.status(404).send({"error": "The requested resouce couldn't be found."});
-})
+    res.status(404).send({"error": "The requested resource couldn't be found."});
+});
 
-app.listen(process.env.PORT, () => {
-    console.log(`This server is now listening on port ${process.env.PORT}. Go give it a whirl!`);
-})
+app.listen(port, () => {
+    console.log(`This server is now listening on port ${port}. Go give it a whirl!`);
+});
